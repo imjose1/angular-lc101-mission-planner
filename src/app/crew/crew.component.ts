24bdea3b2558e8929c3c5d54f591f4ef357ed988
Jose g hernandez <jose_ghj@hotmail.com>
2023-03-02 16:25:34 -0600

    done

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,16 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    {name: "Ellen Ochoa", firstMission: true},
+    
   ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+  
 }
